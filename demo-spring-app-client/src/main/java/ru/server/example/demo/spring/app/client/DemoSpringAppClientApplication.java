cc3445d1b8f5e53893515d8d63f409b7528c723e
Skillbox <bdremb@gmail.com>
2024-04-20 11:45:23 +0300

    init declarative projects

---
+++ demo-spring-app-client/src/main/java/ru/server/example/demo/spring/app/client/DemoSpringAppClientApplication.java
@@ -0,0 +1,13 @@
+package ru.server.example.demo.spring.app.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAppClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringAppClientApplication.class, args);
+    }
+
+}
---
+++ demo-spring-app-client/src/test/java/ru/server/example/demo/spring/app/client/DemoSpringAppClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.server.example.demo.spring.app.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAppClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
